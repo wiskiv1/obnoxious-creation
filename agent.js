3ef146c103b7a3581f54ed554dcb0ed37c6d96e9
wiski <32271164+wiskiv1@users.noreply.github.com>
2021-11-19 21:57:29 +0100

    afstand functie toegevoegd voor social distancing

--- agent.js
+++ agent.js
@@ -4,6 +4,7 @@ class agent {
         this.position = createVector(x, y);
         this.velocity = createVector(0,0);
         this.acceleration = createVector(0,0);
+        this.radius = 30
     }
 
     show() {
@@ -11,14 +12,49 @@ class agent {
         stroke(255);
         strokeWeight(8);
         point(this.position.x, this.position.y);
+        fill(0, 0);
+        strokeWeight(1);
+        //circle(this.position.x, this.position.y, 25)
         pop();
     }
 
     update() {
+        //bereken acceleratie vector
+        
+        //als versnelling 0 is vertragen zodat agenten niet constant aan het rondbewegen zijn
+        if (this.acceleration.mag() == 0) {this.velocity.mult(0.8)}
+
         //voegsnelheid toe aan positie, versnelling aan snelheid en zet versnelling gelijk aan 0
+        //limiteer de snelheid en versnelling van de agent
+        if (this.acceleration.mag() > 8) {this.acceleration.setMag(8);}
         this.position.add(this.velocity);
         this.velocity.add(this.acceleration);
-        this.acceleration.mult(0);
-        //versnelling wordt altijd ge reset omdat we elk frame een unieke versnelling willen op basis van verschillende factoren
+        if (this.velocity.mag() > 4) {this.velocity.setMag(4);}
+        this.acceleration.setMag(0);
+
+        //van wereld een donut maken
+        if (this.position.x > width) {this.position.x = 0;}
+        if (this.position.x < 0) {this.position.x = width;}
+        if (this.position.y > height) {this.position.y = 0;}
+        if (this.position.y < 0) {this.position.y = height;}
+    }
+
+    afstand() {
+        let vector = createVector(0, 0);
+
+        for (let a of agents) {
+            if (a == this) continue;
+
+            let d = dist(a.position.x, a.position.y, this.position.x, this.position.y);
+            if (d < this.radius) {
+                //bereken vector die wijst van a naar this
+                let t = p5.Vector.sub(this.position, a.position);
+                //pas grote van de vector aan op basis van afstand
+                t.mult(this.radius / d);
+                vector.add(t);
+            }
+        }
+
+        this.acceleration.add(vector);
     }
 }
--- sketch.js
+++ sketch.js
@@ -2,18 +2,25 @@ let agents = []
 
 function setup() {
   createCanvas(800, 600);
-  console.rainbow("Hello World!");
+  console.rainbow("Welkom bij mijn simulatie");
 
   frameRate(30);
   //framerate op 30 zetten, wordt makkelijk om later te berekenen hoe lang agents bij elkaar in de buurt zijn in "echte tijd"
-  //als we er van uitgaan dat 1 frame bv 1 sec is
-  //om zelfde reden gaan we er van uit gaan dat 1 pixel 10 cm is
+  //als we er bv van uitgaan dat 1 frame bv 1 sec is
+  //om zelfde reden gaan we er van uit gaan dat 1 pixel bv 10 cm is
 
-  agents.push(new agent(400, 300));
+  for (var i = 0; i < 200; i++) {
+    agents.push(new agent(random(width), random(height))); //maak 100 agents verspreid random over het veld
+  }
 }
 
 function draw() {
   background(51);
-  agents[0].show();
-  agents[0].update();
+  for (let x of agents) {
+    x.show();
+    x.afstand();
+    x.update();
+  }
 }
+
+//function mousePressed() {agents.push(new agent(mouseX, mouseY));}
